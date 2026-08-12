9155c24b72c810626fd7e70998e9402c6884d925
cryptobitter <adityarajput.86046@gmail.com>
2026-08-12 14:29:26 +0530

    Time: 52 ms (9.74%), Space: 35.8 MB (7.91%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k%=len(nums)
+
+        nums.reverse()
+
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
+
+        return nums
